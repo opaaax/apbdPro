59d7ea275b4551b4c4786554498e16a6cba1d3e0
opax <opaaaax@gmail.com>
2025-06-13 16:50:05 +0200

    made some progress

---
+++ APBDProjekt/Models/Oprogramowanie.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace APBDProjekt.Models;
+
+[Table("Oprogramowanie")]
+public class Oprogramowanie
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public required string Nazwa { get; set; }
+    [Required]
+    [DataType("numeric")]
+    [Precision(10, 2)]
+    public double CenaZaRok { get; set; }
+    [Required]
+    [MaxLength(100)]
+    public required string InfoOWersji { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public required string Kategoria { get; set; }
+    public ICollection<Znizka> Znizki { get; set; }
+}
---
+++ APBDProjekt/Models/Znizka.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APBDProjekt.Models;
+
+[Table("Znizka")]
+public class Znizka
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(50)]   
+    public required string Nazwa { get; set; }
+    [Required]
+    public int WartoscWProcentach { set; get; }
+    [Required]
+    public DateTime DataRozpoczecia { get; set; }
+    [Required]
+    public DateTime DataZakonczenia { get; set; }
+    [ForeignKey(nameof(Oprogramowanie))]
+    public int IdOprogramowania { get; set; }
+    public Oprogramowanie Oprogramowanie { get; set; }
+    
+}
